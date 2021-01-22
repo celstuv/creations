880801ff97f44311117f699998a66334d11ba59c
celstuv <celstuv@yahoofr>
2021-01-22 20:05:00 +0100

    Creation de data.js

---
+++ src/components/Data.js
@@ -0,0 +1,75 @@
+import React from 'react';
+export const links = [
+  {
+    id: 1,
+    url: '/',
+    text: 'Home',
+  },
+  {
+    id: 2,
+    url: '/savoirfaire',
+    text: 'Savoir-Faire',
+  },
+  {
+    id: 3,
+    url: '/Blog',
+    text: 'Blog',
+  },
+  {
+    id: 4,
+    url: '/contact',
+    text: 'contact',
+  },
+];
+export const social = [
+  {
+    id: 1,
+    url: 'https://www.facebook.com',
+    icon: <FaFacebook />,
+  },
+  {
+    id: 2,
+    url: 'https://www.twitter.com',
+    icon: <FaTwitter />,
+  },
+  {
+    id: 3,
+    url: 'https://www.linkedin.com',
+    icon: <FaLinkedin />,
+  },
+  {
+    id: 4,
+    url: 'https://www.behance.net',
+    icon: <FaBehance />,
+  },
+];
+export const gallery = [
+  {
+    id: 1,
+    url: 'https://cdn.pixabay.com/photo/2019/11/20/11/15/knitting-4639828__340.jpg',
+  },
+  {
+    id: 2,
+    url: 'https://images.unsplash.com/photo-1602773984044-3ecbed81556d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8cGx1c2glMjB3b29sfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    id: 3,
+    url: 'https://images.unsplash.com/photo-1602685365252-c13f549f1f5f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzY5fHx3b29sfGVufDB8MHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    id: 4,
+    url: 'https://cdn.pixabay.com/photo/2018/01/30/12/04/fabric-3118624__340.jpg',
+  },
+  {
+    id: 5,
+    url: 'https://cdn.pixabay.com/photo/2018/01/12/13/51/red-3078278__340.jpg',
+  },
+  {
+    id: 6,
+    url: 'https://cdn.pixabay.com/photo/2015/07/31/15/02/knit-869221__340.jpg',
+  },
+  {
+    id: 7,
+    url: 'https://images.unsplash.com/photo-1543334270-24bb46642afe?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NTh8fHdvb2x8ZW58MHwwfDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+  },
+]
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import Form from 'react-bootstrap/Form'
@@ -10,29 +9,30 @@ import { Link } from 'react-router-dom';
 
 const Navigation = () => {
 
-  return(
+  return (
     <div className="container-fluid my-3">
-      <ul style={{display: 'flex', justifyContent: "between"}}>
+      <ul style={{ display: 'flex', justifyContent: 'between' }}>
         <Link to="/">
-          <li style={{marginLeft: 5, listStyle: 'none'}}>
+          <li style={{ marginLeft: 5, listStyle: 'none' }}>
             <img alt="logo_boutique" src={logo} className="d-inline-block align-top" />&#160;
             <span>Lyline's Boutique</span>
           </li>
         </Link>
         <Link to="/">
-          <li style={{marginLeft: 10, listStyle: 'none'}}>Accueil</li>
+          <li style={{ marginLeft: 10, listStyle: 'none' }}>Accueil</li>
         </Link>
         <Link to="savoirfaire">
-          <li style={{marginLeft: 10, listStyle: 'none'}}>Savoir-Faire</li>
+          <li style={{ marginLeft: 10, listStyle: 'none' }}>Savoir-Faire</li>
         </Link>
         <Link to="blog">
-          <li style={{marginLeft: 10, listStyle: 'none'}}>Blog</li>
+          <li style={{ marginLeft: 10, listStyle: 'none' }}>Blog</li>
         </Link>
         <Link to="/contact">
-          <li style={{marginLeft: 10, listStyle: 'none'}}>Contact</li>
+          <li style={{ marginLeft: 10, listStyle: 'none' }}>Contact</li>
         </Link>
       </ul>
     </div>
-  )
+  );
 }
+
 export default Navigation;
